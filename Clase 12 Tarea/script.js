e1cf0b96e228618de11807aefedc25a8071b69b1
Axel Del Medico <axeldm07@hotmail.com>
2023-05-30 00:27:52 -0300

    Trabajo final terminado

--- Clase 12 Tarea/script.js
+++ Clase 12 Tarea/script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function saludo() {
-    var nombre = prompt("Por favor, ingresa tu nombre");
+    let nombre = prompt("Por favor, ingresa tu nombre");
   
-    if (nombre === null || nombre === "") {
+    if (nombre === null || nombre === "" || nombre === " ") {
       alert("Por favor, ingresa tu nombre.");
       location.reload();
     } else {
       alert("¡Bienvenido, " + nombre + "!");
     }
-  }
+}
